d60e677eac8e72909009f92bc3152864c5e4ac0d
Kevin Lin <kevinptt0323@gmail.com>
2015-09-17 17:58:59 +0800

    branch gh-pages

---
+++ js/scoreboard.js
@@ -0,0 +1,170 @@
+var Scoreboard = React.createClass({displayName: "Scoreboard",
+  render: function() {
+    return (
+      React.createElement("div", null, 
+        React.createElement("div", null, "Scoreboard Time: ", this.state.stateTime), 
+        React.createElement("table", null, 
+          React.createElement(Header, {settingsUrl: this.props.settingsUrl}), 
+          React.createElement(Ranking, {_status: this.state._status, _teams: this.state._teams})
+        )
+      )
+    );
+  },
+  componentDidMount: function() {
+    this.loadTeams();
+    this.loadStatus();
+    var $elem = $(this.getDOMNode()).children("table");
+    $elem.addClass("ui striped unstackable table");
+    $("#autoReload").change(this.loadStatus);
+  },
+  getInitialState: function() {
+    return { _status: [], stateTime: "----/--/-- --:--:--" };
+  },
+  loadStatus: function() {
+    if( !$("#autoReload").prop('checked') ) return;
+    $("#loader").fadeIn(500);
+    $.ajax({
+      url: this.props.statusUrl,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({_status: data["status"], stateTime: data["generateTime"]});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.statusUrl, status, err.toString());
+      }.bind(this),
+      complete: function() {
+        $("#loader").fadeOut(500);
+        setTimeout(this.loadStatus, 10000);
+      }.bind(this)
+    });
+  },
+  loadTeams: function() {
+    $.ajax({
+      url: this.props.teamsUrl,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({_teams: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.teamsUrl, status, err.toString());
+      }.bind(this)
+    });
+  }
+});
+
+var Header = React.createClass({displayName: "Header",
+  render: function() {
+    var ths = [
+      React.createElement("th", null, "Rank"),
+      React.createElement("th", null, "Name"),
+      React.createElement("th", null, "Solved"),
+      React.createElement("th", null, "Time")
+    ];
+    for(var i=0; i<this.state._settings.problemNum; i++) {
+      ths.push(React.createElement("th", null, String.fromCharCode(65+i)));
+    }
+    return (
+      React.createElement("thead", null, 
+        React.createElement("tr", null, 
+          ths
+        )
+      )
+    );
+  },
+  getInitialState: function() {
+    return { _settings: {} };
+  },
+  componentDidMount: function() {
+    this.loadSettings();
+  },
+  loadSettings: function() {
+    $.ajax({
+      url: this.props.settingsUrl,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({_settings: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.settingsUrl, status, err.toString());
+      }.bind(this)
+    });
+  }
+});
+
+var Ranking = React.createClass({displayName: "Ranking",
+  render: function() {
+    var
+      _status = this.props._status,
+      _teams = this.props._teams;
+
+    _status.sort(function(a, b) {
+      return (b.solvedN-a.solvedN) || (a.totalPenalty-b.totalPenalty);
+    });
+    ranking = [];
+    for(var i=0, j=0; i<_status.length; i++) {
+      if( _status[i].solvedN != _status[j].solvedN || _status[i].totalPenalty != _status[j].totalPenalty )
+        j = i;
+      var teamName = "team"+_status[i].teamID;
+      for(var t in _teams) {
+        if( _teams[t].teamID == _status[i].teamID ) {
+          teamName = _teams[t].name || teamName;
+          console.log(teamName);
+          break;
+        }
+      }
+      ranking.push(React.createElement(Team, {rank: j+1, result: _status[i], teamName: teamName}))
+    }
+    return (
+      React.createElement("tbody", null, 
+        ranking
+      )
+    );
+//  {JSON.stringify(this.state._status, null, '  ')}
+  },
+});
+
+var Team = React.createClass({displayName: "Team",
+  render: function() {
+    var result = this.props.result;
+    var cx = React.addons.classSet;
+    var tds = [
+      React.createElement("td", null, this.props.rank),
+      React.createElement("td", null, this.props.teamName),
+      React.createElement("td", null, result.solvedN),
+      React.createElement("td", null, result.totalPenalty)
+    ];
+    for(var i in result.solved) {
+      var cls_status = cx({
+        'submitted': result.submitN[i]>0,
+        'solved': result.solved[i],
+        'firstBlood': result.firstBlood[i],
+        'pending': result.pending[i]
+      });
+      tds.push(React.createElement("td", {className: cls_status}, React.createElement("i", {className: "icon"}), " ", result.submitN[i]+"/"+(result.solved[i] ? result.penalty[i] : "--")));
+    }
+    return (
+      React.createElement("tr", null, 
+        tds
+      )
+    );
+  },
+  componentDidMount: function() {
+    this.componentDidUpdate();
+  },
+  componentDidUpdate: function() {
+    var $elem = $(this.getDOMNode());
+    $elem.find("i.icon").attr('class', 'icon');
+    $elem.find(".pending:not(.solved) i.icon").attr('class', 'yellow wait icon');
+    $elem.find(".submitted:not(.pending):not(.solved) i.icon").attr('class', 'red close icon');
+    $elem.find(".solved i.icon").attr('class', 'green checkmark icon');
+  }
+});
+
+React.render(
+  React.createElement(Scoreboard, {settingsUrl: "settings.json", teamsUrl: "teams.json", statusUrl: "status.json"}),
+  document.getElementById('scoreboard')
+)
+
